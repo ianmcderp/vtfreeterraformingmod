d55f9f49af6b36a24c6a84510ddece7976d37054
ianmcderp <matthias@riedl.rocks>
2021-05-28 16:37:40 +0200

    Projektdateien hinzufügen.

---
+++ VTFreeTerraformingMod.cs
@@ -0,0 +1,38 @@
+﻿using HarmonyLib;
+using System;
+using System.Reflection;
+using VoxelTycoon;
+using VoxelTycoon.Modding;
+
+namespace VTFreeTerraformingMod
+{
+    public class VTFreeTerraformingMod : Mod
+    {
+        Logger _logger = new Logger<VTFreeTerraformingMod>();
+
+        protected override void OnGameStarted()
+        {
+            try
+            {
+                var harmony = new Harmony("com.github.ianmcderp");
+                Harmony.DEBUG = true;
+                harmony.PatchAll(Assembly.GetExecutingAssembly());
+                harmony.PatchAll();
+            }
+            catch (Exception e)
+            {
+                _logger.LogError("Loading VTFreeTerraformingMod failed");
+                _logger.LogException(e);
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(VoxelTycoon.Tools.ToolHelper), "GetFlattenPrice", new[] { typeof(Xyz), typeof(int) })]
+    class PatchedToolHelper
+    {
+        static void Postfix(ref double __result)
+        {   
+            __result = 0;
+        }
+    }
+}
